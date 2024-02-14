d1039574b499d2062232e13433b60bc6433ce56d
ooeolee <eewooeo@gmail.com>
2024-02-14 20:19:20 +0900

    init

---
+++ server.js
@@ -0,0 +1,16 @@
+require("dotenv").config();
+import { ApolloServer } from "apollo-server";
+import { typeDefs, resolvers } from "./schema";
+
+const server = new ApolloServer({
+  typeDefs,
+  resolvers
+});
+
+const PORT = process.env.PORT;
+
+server
+  .listen(PORT)
+  .then(() =>
+    console.log(` 🪪Server is running on http://localhost:${PORT}🧸 ✅`)
+  );
